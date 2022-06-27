108e47efa0553dd9ec9cfe519c7256202a9499eb
unknown <mdmiraz7589@gmail.com>
2022-06-26 17:07:45 -0930

    add all

---
+++ ___sever_derloy.js
@@ -0,0 +1,27 @@
+/**
+ * ONE TIME for your computer
+ * ------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------
+ * for each project one time
+ * --------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commet. git push
+ * 3. git push heroku main
+ * 4. Go to Heroku dashbord > Current Project> Settings > Reveal Config vars
+ * 5. copy Paste config  vars from your .env file
+ * 6. Make sure you Have whitelsted all ip address to access mongodb
+ * ------------------
+ * UPDATE SERVER With new changes
+ * ------------------
+ * 1. Make changes
+ * 2. make sure you: git add . git commet. git push
+ * 3. git push heroku main
+ * ------------------
+ * Connect server with Clint
+ * ------------------- 
+ * 
+*/
